6c5b99c6fe493b793a151f8c80c1bcc3794975f6
filedesless <filedesless@gmail.com>
2018-12-08 21:44:20 -0500

    added day03 solutions, kinda slow

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Text.Printf (printf)
 import Data.Text (justifyRight, unpack, pack)
 import Day01 (day01a, day01b)
 import Day02 (day02a, day02b)
+import Day03 (day03a, day03b)
 
 run :: (Show a) => Integer  -> String -> (String -> a) -> IO ()
 run day part function =
@@ -15,4 +16,6 @@ main =
   run 1 "a)" day01a >>
   run 1 "b)" day01b >>
   run 2 "a)" day02a >>
-  run 2 "b)" day02b
+  run 2 "b)" day02b >>
+  run 3 "a)" day03a >>
+  run 3 "b)" day03b
--- src/Day03.hs
+++ src/Day03.hs
@@ -1,6 +1,11 @@
 module Day03 where
 
-data Point = Point Int Int deriving (Show, Eq)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Data.List
+
+data Point = Point Int Int deriving (Show, Eq, Ord)
 data Claim = Claim Int Point Point deriving (Show, Eq)
 
 getId :: String -> Int
@@ -23,23 +28,41 @@ getClaim s =
 claims :: String -> [Claim]
 claims = map getClaim . lines
 
-corners :: Claim -> [Point]
-corners (Claim _ (Point x y) (Point w h)) =
-  [Point x y, Point xw y, Point x yh, Point xw yh]
-  where (xw, yh) = (pred x + w, pred y + h)
+pointInClaim :: Point -> Claim -> Bool
+pointInClaim (Point x1 y1) (Claim _ (Point x2 y2) (Point w2 h2)) =
+  x1 >= x2 && x1 < x2 + w2 &&
+  y1 >= y2 && y1 < y2 + h2
 
-pointInClaim :: Claim -> Point -> Bool
-pointInClaim (Claim _ (Point x1 y1) (Point w1 h1)) (Point x2 y2) =
-  x2 >= x1 && x2 < x1 + w1 &&
-  y2 >= y1 && y2 < y1 + h1
+pointsOfClaim :: Claim -> [Point]
+pointsOfClaim (Claim _ (Point x y) (Point w h)) =
+  [ Point a b | a <- [x..x+w-1], b <- [y..y+h-1] ]
+
+contours :: Claim -> [Point]
+contours (Claim _ (Point x y) (Point w h)) =
+  [ Point a b | a <- [x, xw], b <- [y..yh] ] ++
+  [ Point a b | a <- [x..xw], b <- [y, yh] ]
+  where (xw, yh) = (pred x + w, pred y + h)
 
 overlap :: Claim -> Claim -> Bool
 overlap claim1 claim2 =
-  any (pointInClaim claim1) (corners claim2) ||
-  any (pointInClaim claim2) (corners claim1)
+  any (flip pointInClaim claim1) (contours claim2) ||
+  any (flip pointInClaim claim2) (contours claim1)
+
+f :: Set Point -> [Claim] -> Int
+f seen [] = Set.size seen
+f seen (x:xs) = f (Set.union seen points) xs
+  where
+    points = Set.fromList [ point | point <- pointsOfClaim x,
+                            point `Set.notMember` seen,
+                            any (pointInClaim point) xs ]
+
+g :: [Claim] -> Int
+g xs = case find (\x -> not $ any (overlap x) (delete x xs)) xs of
+  Just (Claim i _ _) -> i
+  otherwise -> error "No free claim found"
 
 day03a :: String -> Int
-day03a = error "Not implemented yet"
+day03a = f Set.empty . claims
 
-day03b :: String -> String
-day03b = error "Not implemented yet"
+day03b :: String -> Int
+day03b = g . claims
