fa92d732c856421e5740b7cc692262ca8f34a5e9
filedesless <filedesless@gmail.com>
2018-12-09 12:00:51 -0500

    little optimization and clarification

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,29 +1,18 @@
 module Day03 where
 
-import Data.Set (Set)
-import qualified Data.Set as Set
-
 import Data.List
+import Data.List.Split
 
 data Point = Point Int Int deriving (Show, Eq, Ord)
 data Claim = Claim Int Point Point deriving (Show, Eq)
 
-getId :: String -> Int
-getId w = read (tail w) :: Int
-
-readT :: (String, String) -> (Int, Int)
-readT (s1, s2) = (read s1, read (tail s2))
-
-getCoord :: String -> Point
-getCoord w = (uncurry Point) $ readT (span (/= ',') (init w))
-
-getArea :: String -> Point
-getArea w = (uncurry Point) $ readT (span (/= 'x') w)
-
 getClaim :: String -> Claim
-getClaim s =
-    Claim (getId (head ws)) (getCoord (ws !! 2)) (getArea (ws !! 3))
-    where ws = words s
+getClaim s = Claim cid pos dim
+    where -- #1 @ 257,829: 10x23
+      ws  = filter (not . null) $ splitOneOf "#@,:x " s
+      cid = read $ head ws
+      pos = Point (read (ws !! 1)) (read (ws !! 2))
+      dim = Point (read (ws !! 3)) (read (ws !! 4))
 
 claims :: String -> [Claim]
 claims = map getClaim . lines
@@ -33,8 +22,8 @@ pointInClaim (Point x1 y1) (Claim _ (Point x2 y2) (Point w2 h2)) =
   x1 >= x2 && x1 < x2 + w2 &&
   y1 >= y2 && y1 < y2 + h2
 
-pointsOfClaim :: Claim -> [Point]
-pointsOfClaim (Claim _ (Point x y) (Point w h)) =
+points :: Claim -> [Point]
+points (Claim _ (Point x y) (Point w h)) =
   [ Point a b | a <- [x..x+w-1], b <- [y..y+h-1] ]
 
 contours :: Claim -> [Point]
@@ -48,21 +37,14 @@ overlap claim1 claim2 =
   any (flip pointInClaim claim1) (contours claim2) ||
   any (flip pointInClaim claim2) (contours claim1)
 
-f :: Set Point -> [Claim] -> Int
-f seen [] = Set.size seen
-f seen (x:xs) = f (Set.union seen points) xs
-  where
-    points = Set.fromList [ point | point <- pointsOfClaim x,
-                            point `Set.notMember` seen,
-                            any (pointInClaim point) xs ]
-
-g :: [Claim] -> Int
-g xs = case find (\x -> not $ any (overlap x) (delete x xs)) xs of
-  Just (Claim i _ _) -> i
-  otherwise -> error "No free claim found"
+nonOverlappingId :: [Claim] -> Int
+nonOverlappingId xs =
+  case find (\x -> not $ any (overlap x) (delete x xs)) xs of
+    Just (Claim i _ _) -> i
+    otherwise -> error "No free claim found"
 
 day03a :: String -> Int
-day03a = f Set.empty . claims
+day03a = length . filter ((> 1) . length) . group . sort . concatMap points . claims
 
 day03b :: String -> Int
-day03b = g . claims
+day03b = nonOverlappingId . claims
