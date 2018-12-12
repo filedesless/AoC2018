8d763e1fd89e5ec0ee687052094aeff22a8602cf
filedesless <filedesless@gmail.com>
2018-12-12 04:59:44 -0500

    parse the example file

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,14 +1,40 @@
 module Day04 where
 
 import Data.Time
+import Data.List
+import Data.List.Split
 
 data Shift = Shift { guardId :: Int
                    , shiftDay :: Day
                    , sleepIntervals :: [(Int, Int)] -- [min,max[
                    } deriving (Show, Eq)
 
+
+linesOfDay :: [String] -> Day -> [String]
+linesOfDay sorted day = filter (showGregorian day `isInfixOf`) sorted
+
+getGid :: [String] -> Day -> Int
+getGid sorted day = case find (isInfixOf "Guard") $ linesOfDay sorted day of
+  Just line -> read $ splitOneOf "# " line !! 4
+  Nothing -> getGid (reverse sorted) $ pred day
+
+getSleepIntervals :: [String] -> [(Int, Int)]
+getSleepIntervals sorted = [ (read fall, read wake) | (fall, wake) <- group2 xs ]
+  where
+    group2 [] = []
+    group2 (fall:wake:res) = (fall, wake) : group2 res
+    xs = [ drop 3 $ take 5 $ words line !! 1 |
+           line <- sorted, " 00:" `isInfixOf` line,
+           "falls" `isInfixOf` line || "wakes" `isInfixOf` line ]
+
 getShifts :: String -> [Shift]
-getShifts = error "Not implemented yet"
+getShifts input = [ Shift (getGid sorted day) day (sleeps day) | day <- daysOf sorted ]
+  where
+    sleeps = getSleepIntervals . linesOfDay sorted
+    daysOf = nub . map (extractDay . getDay)
+    getDay = splitOn "-" . head . words
+    sorted = sort $ lines input
+    extractDay [x, y, z] = fromGregorian (read $ drop 1 x) (read y) (read z)
 
 day04a :: String -> Int
 day04a = error "Not implemented yet"
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -4,16 +4,13 @@ import Day04
 import Test.Hspec (Spec, describe, it, shouldBe)
 import Data.Time
 
-aDay :: Int -> Int -> Day
-aDay = fromGregorian 1518
-
 expectedShifts :: [Shift]
 expectedShifts = [
-  Shift 10 (aDay 11 01) [(05, 25), (30, 55)],
-  Shift 99 (aDay 11 02) [(40, 50)],
-  Shift 10 (aDay 11 03) [(24, 29)],
-  Shift 99 (aDay 11 04) [(36, 46)],
-  Shift 99 (aDay 11 05) [(45, 55)]
+  Shift 10 (fromGregorian 1518 11 01) [(05, 25), (30, 55)],
+  Shift 99 (fromGregorian 1518 11 02) [(40, 50)],
+  Shift 10 (fromGregorian 1518 11 03) [(24, 29)],
+  Shift 99 (fromGregorian 1518 11 04) [(36, 46)],
+  Shift 99 (fromGregorian 1518 11 05) [(45, 55)]
   ]
 
 spec :: Spec
