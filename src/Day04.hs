ebd5500f1b4fb79e832f78d44f9b91fdd887fc8b
filedesless <filedesless@gmail.com>
2018-12-22 16:57:01 -0500

    light cleanup

--- src/Day04.hs
+++ src/Day04.hs
@@ -10,15 +10,6 @@ data Shift = Shift { guardId :: Int
                    , sleepIntervals :: [(Int, Int)] -- [min,max[
                    } deriving (Show, Eq)
 
-
-linesOfDay :: [String] -> Day -> [String]
-linesOfDay sorted day = filter (showGregorian day `isInfixOf`) sorted
-
-getGid :: [String] -> Day -> Int
-getGid sorted day = case find (isInfixOf "Guard") $ linesOfDay sorted day of
-  Just line -> read $ splitOneOf "# " line !! 4
-  Nothing -> getGid (reverse sorted) $ pred day
-
 getSleepIntervals :: [String] -> [(Int, Int)]
 getSleepIntervals sorted = [ (read fall, read wake) | (fall, wake) <- group2 xs ]
   where
@@ -28,14 +19,18 @@ getSleepIntervals sorted = [ (read fall, read wake) | (fall, wake) <- group2 xs 
            line <- sorted, " 00:" `isInfixOf` line,
            "falls" `isInfixOf` line || "wakes" `isInfixOf` line ]
 
-getShifts :: String -> [Shift]
-getShifts input = [ Shift (getGid sorted day) day (sleeps day) | day <- daysOf sorted ]
+getShifts' :: [String] -> [Shift]
+getShifts' [] = []
+getShifts' (h:t) = Shift gid day (getSleepIntervals today) : getShifts' after
   where
-    sleeps = getSleepIntervals . linesOfDay sorted
-    daysOf = nub . map (extractDay . getDay)
-    getDay = splitOn "-" . head . words
-    sorted = sort $ lines input
+    gid = read $ splitOneOf "# " h !! 4
+    day = if "00:" `isInfixOf` h then getDay h else succ (getDay h)
+    getDay = extractDay . splitOn "-" . head . words
     extractDay [x, y, z] = fromGregorian (read $ drop 1 x) (read y) (read z)
+    (today, after) = span (not . elem '#') t
+
+getShifts :: String -> [Shift]
+getShifts = getShifts' . sort . lines
 
 day04a :: String -> Int
 day04a s = minute * guardId chosen
