33df8cf0b4de3c505365496186d99c4f81c6009c
filedesless <filedesless@gmail.com>
2018-12-30 18:17:26 -0500

    cleaned up

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,27 +1,21 @@
 module Day05 where
 
 import Data.List
-import Data.Function( on )
 
 react :: Char -> Char -> Bool
 react x y = 32 == abs (fromEnum x - fromEnum y)
 
-removePairs :: String -> String
-removePairs [ ] = [ ]
-removePairs [x] = [x]
-removePairs (x:y:rest)
-  | react x y = removePairs rest
-  | otherwise = x : removePairs (y : rest)
-
 reactAll :: String -> String
-reactAll s
-  | all ((== 1) . length) $ groupBy react s = delete '\n' s
-  | otherwise = reactAll $ removePairs s
+reactAll (x:xs) = foldl fn [x] $ delete '\n' xs
+  where fn [] v = [v]
+        fn (h:t) v
+          | react h v = t
+          | otherwise = v : h : t
 
 day05a :: String -> Int
 day05a = length . reactAll
 
 day05b :: String -> Int
-day05b s = minimum $ map (length . reactAll . possibility) ['a'..'z']
-  where
-    possibility l = [ c | c <- (delete '\n' s), not $ react l c, c /= l ]
+day05b s = minimum possibilities
+  where possibilities = map (length . reactAll . possibility) ['a'..'z']
+        possibility l = [ c | c <- (delete '\n' s), not $ react l c, c /= l ]
