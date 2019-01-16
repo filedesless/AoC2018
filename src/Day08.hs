990abc56dd28a741a2d2d03bbcf01e84b587ff48
filedesless <filedesless@gmail.com>
2019-01-16 12:33:42 -0500

    apparently valid rose tree parser

--- src/Day08.hs
+++ src/Day08.hs
@@ -3,10 +3,16 @@ module Day08 where
 import Data.Tree
 
 readNumbers :: String -> [Int]
-readNumbers s = undefined
+readNumbers s = [ read c :: Int | c <- words s ]
 
 buildTree :: [Int] -> (Int, Tree [Int])
-buildTree s  = undefined
+buildTree (n:m:rest) = (total + m + 2, Node (take m $ drop total rest) forest)
+  where
+    (total, forest) =
+      foldl (\(total, forest) _ ->
+               let todo = drop total $ take (length rest - m) rest in
+                 let (i, Node label sub) = buildTree todo in
+                   (total + i, forest ++ [Node label sub])) (0, [])  [1..n]
 
 day08a :: String -> Int
 day08a s = undefined
