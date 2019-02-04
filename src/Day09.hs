700949fa5d9189bd5c3eec718f012be1fd689dde
filedesless <filedesless@gmail.com>
2019-02-04 18:56:26 -0500

    Revert "optimization, doesnt help much"
    This reverts commit 293b3df8967964ad0b6e2f585c5c56823f66d865.

--- src/Day09.hs
+++ src/Day09.hs
@@ -7,16 +7,16 @@ data Game = Game { getScores :: CList Int
                  , getMarble :: CList Int
                  } deriving (Show, Eq)
 
-turn :: Game -> (Int, Int) -> Game
-turn (Game scores marble) (i, i')
-  | i' == 0   = Game (rotR score) $ removeR lefty
-  | otherwise = Game (rotR scores) $ insertL i $ rotR marble
+turn :: Game -> Int -> Game
+turn (Game scores marble) i
+  | mod i 23 == 0  = Game (rotR score) $ removeR lefty
+  | otherwise      = Game (rotR scores) $ insertL i $ rotR marble
   where
     score = update (sum $ catMaybes [Just i, focus scores, focus lefty]) scores
     lefty = rotNL 7 marble
 
 highest :: Int -> Int -> Int
-highest n m = foldlR max 0 . getScores . foldl turn game $ zip [1..m] (cycle [0..22])
+highest n m = foldlR max 0 . getScores $ foldl turn game [1..m]
   where game = Game (fromList $ replicate n 0) (singleton 0)
 
 getInput :: String -> (Int, Int)
